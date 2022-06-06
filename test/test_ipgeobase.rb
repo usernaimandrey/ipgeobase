3661d49b9e9b19d604319ce77dacf06640266c40
usernaimandrey <anshlyapnikov@yandex.ru>
2022-06-06 01:57:35 +0000

    test

--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
+require_relative '../lib/ipgeobase.rb'
 
 class TestIpgeobase < Minitest::Test
+
+  def test_test
+    assert(Ipgeobase.lookup(8) == 8)
+  end
 end
